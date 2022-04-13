70f1d374280e6ba80eb26de1cfcb868255775ef9
Brayan Farro <farrocachaybrayan@gmail.com>
2022-04-13 16:34:59 -0500

    modificando rutas

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,16 @@
 const btn_menu = document.querySelector(".fa-solid")
+const nav = document.querySelector(".container-nav")
 const menu = document.querySelector(".menu")
 
 btn_menu.addEventListener("click", ()=> {
-    menu.classList.toggle("active")
+
     btn_menu.classList.toggle("girar")
+    menu.classList.toggle("active")
+    nav.classList.toggle("color")
+
+})
+menu.addEventListener("click", ()=> {
+    btn_menu.classList.remove("girar")
+    menu.classList.remove("active")
+    nav.classList.remove("color")
 })
